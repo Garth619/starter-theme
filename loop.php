73fee8b76a52f98af0a675d0b6c067a8af4b95e8
Garrett Cullen <Garth619@users.noreply.github.com>
2017-09-04 18:17:15 -0700

    push

--- loop.php
+++ loop.php
@@ -18,10 +18,7 @@
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-<?php while ( have_posts() ) : the_post(); ?>
-
-
-		<?php the_content();?>
+	<?php the_content();?>
 			
 		<?php edit_post_link( __( 'Edit', 'twentyten' ), '', '' ); ?>
 			
