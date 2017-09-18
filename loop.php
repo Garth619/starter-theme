b6e14e94e44c2321c283aaa4ba9bc5465e74fbb9
Garrett Cullen <Garth619@users.noreply.github.com>
2017-09-18 10:53:21 -0700

    push

--- loop.php
+++ loop.php
@@ -20,7 +20,7 @@
 
 	<?php the_content();?>
 			
-		<?php edit_post_link( __( 'Edit', 'twentyten' ), '', '' ); ?>
+		<?php edit_post_link( __( 'Edit'), '', '' ); ?>
 			
 		
 <?php endwhile; // end of loop ?>
@@ -33,9 +33,9 @@
 	
 	<div id="nav-below" class="navigation">
 		
-		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyten' ) ); ?></div>
+		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts' ) ); ?></div>
 			
-		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
+		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>') ); ?></div>
 	
 	</div><!-- #nav-below -->
 
