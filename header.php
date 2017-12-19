4acdcd06209e03e63bd32ebaca4a65e945053659
Garrett Cullen <Garth619@users.noreply.github.com>
2017-12-19 12:49:05 -0800

    push

--- header.php
+++ header.php
@@ -26,7 +26,10 @@
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+
 <?php wp_head(); ?>
+
 </head>
 
 <body <?php body_class(); ?>>
