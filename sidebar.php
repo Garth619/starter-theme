7ac8f6569ad7589e10545276a77f6a41d291bea8
Garrett Cullen <Garth619@users.noreply.github.com>
2018-02-11 16:02:48 -0800

    push

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,8 @@
 
-
-
-	<?php dynamic_sidebar( 'sidebar' ); ?>
+	<?php // dynamic_sidebar( 'sidebar' ); ?>
+			
+	<?php // dynamic_sidebar( 'category_sidebar' ); ?>
+	
+	<?php // dynamic_sidebar( 'archive_sidebar' ); ?>		
+			
 			
